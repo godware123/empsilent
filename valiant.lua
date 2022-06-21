537b2dce938474ca9882134ae0e670623713c06c
godware123 <94326058+godware123@users.noreply.github.com>
2022-06-21 11:17:44 +0100

    Add files via upload

---
+++ valiant.lua
@@ -0,0 +1,23 @@
+--[[
+zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+z:::::::::::::::zz:::::::::::::::zz:::::::::::::::z
+z::::::::::::::z z::::::::::::::z z::::::::::::::z 
+zzzzzzzz::::::z  zzzzzzzz::::::z  zzzzzzzz::::::z  
+      z::::::z         z::::::z         z::::::z   
+     z::::::z         z::::::z         z::::::z    
+    z::::::z         z::::::z         z::::::z     
+   z::::::z         z::::::z         z::::::z      
+  z::::::zzzzzzzz  z::::::zzzzzzzz  z::::::zzzzzzzz
+ z::::::::::::::z z::::::::::::::z z::::::::::::::z
+z:::::::::::::::zz:::::::::::::::zz:::::::::::::::z
+zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
+                                                   
+--]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/godware123/empsilent/main/valiantzz"))()
+
+
+local hotkey = "t" 
+local accomidationfactor = 0.122
+getgenv().ValiantAimHacks.FOV = 60
